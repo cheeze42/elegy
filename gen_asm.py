234458d8336b9d4bb834b41dfa169ffacaa2ba02
cheeze42 <dudewithtude42@gmail.com>
2023-04-14 18:01:08 -0400

    Fixed timings to line up with my monitor

--- gen_asm.py
+++ gen_asm.py
@@ -56,36 +56,19 @@ with open("out.asm", 'w+') as asm_file:
     asm_file.write("vga_loop:\n")
 
     # Last part of the previous frame
-    for _ in range(128//cpp - 12):
+    for _ in range(120//cpp - 4):
         asm_file.write("  nop\n")
 
     # Pixel rows
-    asm_file.write("  ldi r30, 0\n")  # Load base image pointer
-    asm_file.write("  ldi r31, 0\n")
     asm_file.write(f"  ldi r16, {480//stretch}\n")  # Outer row counter
     asm_file.write(f"  ldi r17, {stretch}\n")
-    asm_file.write("  lpm r18, Z+\n")  # Load the first pixel from memory
+    asm_file.write("  ldi r20, (1<<SPE)|(1<<MSTR)\n")
+    asm_file.write("  out SPCR, r20\n")
     asm_file.write("pixel_row_loop:\n")
     asm_file.write("pixel_row_inner:\n")
     # 640 pixel visible area
     # Enable SPI
-    asm_file.write("  ldi r20, (1<<SPE)|(1<<MSTR)\n")
-    asm_file.write("  out SPCR, r20\n")
-    asm_file.write("  nop\n")
-    asm_file.write("  nop\n")
-    asm_file.write("  nop\n")
-    asm_file.write("  nop\n")
-    asm_file.write("  nop\n")
-    asm_file.write("  nop\n")
-    asm_file.write("  nop\n")
-    asm_file.write("  nop\n")
-    asm_file.write("  nop\n")
-    asm_file.write("  nop\n")
-    asm_file.write("  nop\n")
-    asm_file.write("  nop\n")
-    asm_file.write("  nop\n")
-    asm_file.write("  nop\n")
-    for _ in range(160//8 - 1):
+    for _ in range(160//8):
         asm_file.write("  out SPDR, r19\n")  # Write the pixel out
         asm_file.write("  nop\n")
         asm_file.write("  nop\n")
@@ -105,31 +88,33 @@ with open("out.asm", 'w+') as asm_file:
     asm_file.write("  ldi r20, (1<<MSTR)\n")
     asm_file.write("  out SPCR, r20\n")
     asm_file.write("  cbi PORTB, 3\n")
-    # 24 pixel front porch
-    for _ in range(24//cpp - 4):
+    # 16 pixel front porch
+    for _ in range(16//cpp - 4):
         asm_file.write("  nop\n")
-    # 40 pixel sync pulse
+    # 64 pixel sync pulse
     asm_file.write("  cbi PORTD, 0\n")  # Start the horizontal pulse
-    for _ in range(40//cpp - 2):
+    for _ in range(64//cpp - 2):
         asm_file.write("  nop\n")
-    # 128 pixel back porch
+    # 120 pixel back porch
     asm_file.write("  sbi PORTD, 0\n")  # Stop the horizontal pulse
-    for _ in range(128//cpp - 14):
+    for _ in range(120//cpp - 13):
         asm_file.write("  nop\n")
-    asm_file.write("  dec r17\n")
-    asm_file.write("  breq pixel_row_inner_done\n")
-    asm_file.write(f"  subi r30, low({640//stretch})\n")
-    asm_file.write(f"  sbci r31, high({640//stretch})\n")
-    asm_file.write("  lpm r18, Z+\n")
-    asm_file.write("  nop\n")
-    asm_file.write("  nop\n")
-    asm_file.write("  jmp pixel_row_inner\n")
+    asm_file.write("  dec r17\n")                         # 1
+    asm_file.write("  breq pixel_row_inner_done\n")       # 1/2
+    asm_file.write(f"  subi r30, low({640//stretch})\n")  # 1
+    asm_file.write(f"  sbci r31, high({640//stretch})\n") # 1
+    asm_file.write("  ldi r20, (1<<SPE)|(1<<MSTR)\n")     # 1
+    asm_file.write("  out SPCR, r20\n")                   # 1
+    asm_file.write("  nop\n")                             # 1
+    asm_file.write("  nop\n")                             # 1
+    asm_file.write("  jmp pixel_row_inner\n")             # 3
     asm_file.write("pixel_row_inner_done:\n")
-    asm_file.write(f"  ldi r17, {stretch}\n")
-    asm_file.write("  dec r16\n")
-    asm_file.write("  breq pixel_row_loop_done\n")
-    asm_file.write("  lpm r18, Z+\n")
-    asm_file.write("  jmp pixel_row_loop\n")
+    asm_file.write(f"  ldi r17, {stretch}\n")             # 1
+    asm_file.write("  dec r16\n")                         # 1
+    asm_file.write("  breq pixel_row_loop_done\n")        # 1/2
+    asm_file.write("  ldi r20, (1<<SPE)|(1<<MSTR)\n")     # 1
+    asm_file.write("  out SPCR, r20\n")                   # 1
+    asm_file.write("  jmp pixel_row_loop\n")              # 3
     asm_file.write("pixel_row_loop_done:\n")
     asm_file.write("  nop\n")
     asm_file.write("  nop\n")
@@ -137,28 +122,20 @@ with open("out.asm", 'w+') as asm_file:
     asm_file.write("  nop\n")
 
     # Vertical front porch
-    asm_file.write("  ldi r16, 24\n")
-    asm_file.write("vertical_front_porch_loop:\n")
-    # 640 pixel visible area + 24 pixel front porch
-    for _ in range(640//cpp + 24//cpp):
+    # 640 pixel visible area + 16 pixel front porch
+    for _ in range(640//cpp + 16//cpp):
         asm_file.write("  nop\n")
-    # 40 pixel sync pulse
+    # 64 pixel sync pulse
     asm_file.write("  cbi PORTD, 0\n")  # Start the horizontal pulse
-    for _ in range(40//cpp - 2):
+    for _ in range(64//cpp - 2):
         asm_file.write("  nop\n")
-    # 128 pixel back porch
+    # 120 pixel back porch
     asm_file.write("  sbi PORTD, 0\n")  # Stop the horizontal pulse
-    for _ in range(128//cpp - 7):
+    for _ in range(120//cpp - 2):
         asm_file.write("  nop\n")
-    asm_file.write("  dec r16\n")
-    asm_file.write("  breq vertical_front_porch_loop_done\n")
-    asm_file.write("  jmp vertical_front_porch_loop\n")
-    asm_file.write("vertical_front_porch_loop_done:\n")
-    asm_file.write("  nop\n")
-    asm_file.write("  nop\n")
 
     # Vertical sync
-    for row_idx in range(2):
+    for row_idx in range(3):
         # 640 pixel visible area + 24 pixel front porch
         if row_idx == 0:
             asm_file.write("  cbi PORTD, 1\n")  # Start the vertical pulse
@@ -167,33 +144,33 @@ with open("out.asm", 'w+') as asm_file:
             asm_file.write("  nop\n")
         for _ in range(640//cpp + 24//cpp - 2):
             asm_file.write("  nop\n")
-        # 40 pixel horizontal sync pulse
+        # 64 pixel horizontal sync pulse
         asm_file.write("  cbi PORTD, 0\n")  # Start the horizontal pulse
-        for _ in range(40//cpp - 2):
+        for _ in range(64//cpp - 2):
             asm_file.write("  nop\n")
-        # 128 pixel back porch
+        # 120 pixel back porch
         asm_file.write("  sbi PORTD, 0\n")  # Stop the horizontal pulse
-        for _ in range(128//cpp - 3):
+        for _ in range(120//cpp - 3):
             asm_file.write("  nop\n")
-        if row_idx == 0:
+        if row_idx != 2:
             asm_file.write("  nop\n")
         else:
-            asm_file.write("  ldi r16, 28\n")
+            asm_file.write("  ldi r16, 15\n")
    
-    # Vertical back porch, 29 lines total
-    # Lines 0 - 28
+    # Vertical back porch, 16 lines total
+    # Lines 0 - 16
     asm_file.write("  sbi PORTD, 1\n")  # Stop the vertical pulse
     asm_file.write("  vertical_back_porch_loop:\n")
     # 640 pixel visible area + 24 pixel front porch
     for _ in range(640//cpp + 24//cpp - 2):
         asm_file.write("  nop\n")
-    # 40 pixel sync pulse
+    # 64 pixel sync pulse
     asm_file.write("  cbi PORTD, 0\n")  # Start the horizontal pulse
-    for _ in range(40//cpp - 2):
+    for _ in range(64//cpp - 2):
         asm_file.write("  nop\n")
-    # 128 pixel back porch
+    # 120 pixel back porch
     asm_file.write("  sbi PORTD, 0\n")  # Stop the horizontal pulse
-    for _ in range(128//cpp - 7):
+    for _ in range(120//cpp - 7):
         asm_file.write("  nop\n")
     asm_file.write("  dec r16\n")
     asm_file.write("  breq vertical_back_porch_loop_done\n")
@@ -202,14 +179,14 @@ with open("out.asm", 'w+') as asm_file:
     asm_file.write("  jmp vertical_back_porch_loop\n")
     asm_file.write("vertical_back_porch_loop_done:\n")
 
-    # Line 28
-    # 640 pixel visible area + 24 pixel front porch
-    for _ in range(640//cpp + 24//cpp + 2):
+    # Line 16
+    # 640 pixel visible area + 16 pixel front porch
+    for _ in range(640//cpp + 16//cpp + 2):
         asm_file.write("  nop\n")
-    # 40 pixel sync pulse
+    # 64 pixel sync pulse
     asm_file.write("  cbi PORTD, 0\n")  # Start the horizontal pulse
-    for _ in range(40//cpp - 2):
+    for _ in range(64//cpp - 2):
         asm_file.write("  nop\n")
-    # 128 pixel back porch
+    # 120 pixel back porch
     asm_file.write("  sbi PORTD, 0\n")  # Stop the horizontal pulse
     asm_file.write("  jmp vga_loop\n")  # Restart the loop
